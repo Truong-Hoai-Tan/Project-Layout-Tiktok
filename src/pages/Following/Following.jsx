93c79169253438da45da64555449c93733dd0b3c
Truong-Hoai-Tan <truonghoaitanag@gmail.com>
2024-05-01 10:34:07 +0700

    Upload project

---
+++ src/pages/Following/Following.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const Following = () => {
+  return (
+    <h2>Following page</h2>
+  )
+}
+
+export default Following
---
+++ src/pages/Upload/Upload.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const Upload = () => {
+  return (
+      <h2>Upload page</h2>
+  )
+}
+
+export default Upload
